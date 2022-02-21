dbf79fbeef146d8f8be1b6109aa04aac03fa0b93
Luis Alfonso <luisalfonsomartinezmartinez@outlook.es>
2022-02-20 20:48:59 -0600

    Agrregamos validaciones y js al login y al registro

--- validContra.js
+++ validContra.js
@@ -13,4 +13,22 @@ var check = function() {
         document.getElementById('message').style.color = 'red';
         document.getElementById('message').innerHTML = 'Favor de llenar los campos';
     }
+    if(document.getElementById('confirm_password').value== null || 
+    document.getElementById('confirm_password').value== ""){
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'Favor de llenar los campos';
+    }
 }      
+
+var checkLogin = function(){
+    if(document.getElementById('usuarioLogin').value == null ||
+        document.getElementById('usuarioLogin').value == ""){
+            document.getElementById('msLogin').style.color= 'red';
+        document.getElementById('msLogin').innerHTML ='Favor llenar campos';
+        }
+    if(document.getElementById('passwordLogin').value == null||
+    document.getElementById('passwordLogin').value == ""){
+        document.getElementById('msLogin').style.color= 'red';
+        document.getElementById('msLogin').innerHTML ='Favor llenar campos';
+    }
+}
